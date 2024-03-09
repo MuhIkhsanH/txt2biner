8a2876f95f5a246ad11c9dd2731c052a54461af3
MuhIkhsanH <153172456+MuhIkhsanH@users.noreply.github.com>
2024-03-09 16:13:56 +0700

    Update txt2biner.py

--- txt2biner.py
+++ txt2biner.py
@@ -2,7 +2,7 @@ def text_to_binary(text):
     binary_data = ''.join(format(ord(char), '08b') for char in text)
     return binary_data
 
-# Panggil fungsi text_to_binary dengan teks yang ingin Anda konversi
+
 text = str(input("Masukkan Text:"))
 binary_representation = text_to_binary(text)
 print("Teks:", text)
